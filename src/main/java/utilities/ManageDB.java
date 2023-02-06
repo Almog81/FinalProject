2bd9c2c014cbcd63c219d182a78e9a20882a79c0
Almog81 <almog81@gmail.com>
2023-02-06 20:31:31 +0200

    Add Description for Methods utilities

--- src/main/java/utilities/ManageDB.java
+++ src/main/java/utilities/ManageDB.java
@@ -3,6 +3,14 @@ package utilities;
 import java.sql.DriverManager;
 
 public class ManageDB extends CommonOps{
+    /*
+    Method Name: openConnection
+    Method Description: Open a connection to a database using the specified URL, user and password
+    Method Parameters: String dbURL - the URL of the database,
+                       String user - the username to use for the connection,
+                       String pass - the password to use for the connection.
+    Method Return: None
+     */
     public static void openConnection (String dbURL, String user, String pass){
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -13,6 +21,12 @@ public class ManageDB extends CommonOps{
         }
     }
 
+    /*
+    Method Name: closeConnection
+    Method Description: Close the connection to a database
+    Method Parameters: None
+    Method Return: None
+     */
     public static void closeConnection(){
         try {
             con.close();
--- src/main/java/utilities/ManageDDT.java
+++ src/main/java/utilities/ManageDDT.java
@@ -10,11 +10,40 @@ import java.util.List;
 
 public class ManageDDT extends CommonOps{
 
+    /*
+    Method Name: getDataObject
+    Method Description: This method returns data from a CSV file as an Object array.
+    Method Parameters: None
+    Method Return: Object[][]
+     */
     @DataProvider(name = "DataProvider-Search")
     public Object[][] getDataObject(){
         return getDataFromCSV(getData("DDTFile") + "MenuSearchOption.csv");
     }
 
+    /*
+    Method Name: getDataFromCSV
+    Method Description: This method returns the data from a CSV file as an Object array.
+    Method Parameters: filePath (String)
+    Method Return: Object[][]
+     */
+    public static Object[][] getDataFromCSV(String filePath){
+        List<String> csvData = readCSV(filePath);
+        Object[][] data = new Object[csvData.size()][2];
+
+        for (int i = 0; i< csvData.size(); i++){
+            data[i][0] = csvData.get(i).split(",")[0];
+            data[i][1] = csvData.get(i).split(",")[1];
+        }
+        return data;
+    }
+
+    /*
+    Method Name: readCSV
+    Method Description: This method reads a CSV file and returns its data as a list of strings.
+    Method Parameters: scvFile (String)
+    Method Return: List<String>
+    */
     public static List<String> readCSV(String scvFile){
         List<String> lines = null;
         File file = new File(scvFile);
@@ -24,18 +53,6 @@ public class ManageDDT extends CommonOps{
             System.out.println("See: " + e);
         }
         return lines;
-    }
-
-
-    public static Object[][] getDataFromCSV(String filePath){
-        List<String> csvData = readCSV(filePath);
-        Object[][] data = new Object[csvData.size()][2]; //מספר העמודות שלי שווה 2 ומספר השורות שלי יכול לשתנות
-
-        for (int i = 0; i< csvData.size(); i++){
-            data[i][0] = csvData.get(i).split(",")[0];
-            data[i][1] = csvData.get(i).split(",")[1];
-        }
-        return data;
     }
 
 }
